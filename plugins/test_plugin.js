dff8179bbd4b8467cf1d25d9f3b5d19cd34698b1
akashskypatel <akashskypatel@gmail.com>
2025-04-29 14:37:28 -0700

    init

---
+++ plugins/test_plugin.js
@@ -0,0 +1,12 @@
+const pluginName = () => 'TestPlugin';
+const pluginVersion = () => 'v0';
+const validate = () => true;
+async function asyncTest() {
+  try {
+    const response = await fetch('https://httpbin.org/get');
+    return response.status;
+  } catch (e) {
+    console.error('Error in asyncTest:', e);
+    return {};
+  }
+}
